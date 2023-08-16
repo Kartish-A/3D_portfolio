261a61be1b1d9555082cae06722110509b9d828d
Abdoulfatah Kartish <abdoulfatah.kartish@msg.group>
2023-08-16 14:58:44 +0200

    mb-animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,7 +37,7 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className="w-3 h-3 rounded-full bg-[#00E5FF] mb-1"
+              className="w-3 h-3 rounded-full bg-[#00E5FF] mb-2"
             />
           </div>
         </a>
