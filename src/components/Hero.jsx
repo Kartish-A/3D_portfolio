13efc70a55423e8284e81ed00e6c3d852a30098c
Abdoulfatah Kartish <abdoulfatah.kartish@msg.group>
2023-08-16 13:16:43 +0200

    responsiveness iPad

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -48,7 +48,7 @@ const Hero = () => {
       </div>
         <ComputersCanvas />
 
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute xs:bottom-10 md:bottom-20 lg:bottom-30 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
