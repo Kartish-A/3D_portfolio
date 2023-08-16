ec447698901683bc1b76941255ea7d0b7e1c1296
Abdoulfatah Kartish <abdoulfatah.kartish@msg.group>
2023-08-16 13:38:19 +0200

    responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -6,33 +6,10 @@ import { ComputersCanvas } from "./canvas";
 
 
 const Hero = () => {
-  const [isMobile, setIsMobile] = useState(false);
-  useEffect(() => {
-    // Add a listener for changes to the screen size
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-    // Set the initial value of the `isMobile` state variable
-    setIsMobile(mediaQuery.matches);
-
-    // Define a callback function to handle changes to the media query
-    const handleMediaQueryChange = (event) => {
-      setIsMobile(event.matches);
-    };
-
-    // Add the callback function as a listener for changes to the media query
-    mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-    // Remove the listener when the component is unmounted
-    return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange);
-    };
-  }, []);
 
   return (
     <section className="relative w-full h-screen mx-auto">
-      <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
-      >
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#00E5FF]"></div>
           <div className="w-1 sm:h-80 h-40 vivid-gradient" />
@@ -46,9 +23,9 @@ const Hero = () => {
           </p>
         </div>
       </div>
-        <ComputersCanvas />
+      <ComputersCanvas />
 
-      <div className="absolute xs:bottom-10 md:bottom-20 lg:bottom-30 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute bottom-32 xs:bottom-12 md:bottom-24 w-full flex justify-center items-center border border-yellow-400">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
