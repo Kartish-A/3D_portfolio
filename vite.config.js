1fc3e51db3ba23f652db1c57f2e4c0996afdcdeb
Abdoulfatah Kartish <abdoulfatah.kartish@msg.group>
2023-08-21 19:18:31 +0200

    base changed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './abd_portfolio',
+  base: '/abd_portfolio/',
   plugins: [react()],
 })
