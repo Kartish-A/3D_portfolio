9fbbd91305c9cf59d3e3467eda6fd523afea7c7e
Abdoulfatah Kartish <abdoulfatah.kartish@msg.group>
2023-09-04 17:10:54 +0200

    merge changes from main into gh-pages 2

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: '/3D_portfolio/',
 })
-=======
-  base: "/3D_portfolio/",
-});
->>>>>>> main
