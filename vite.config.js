c59d0c1a5c150f4a2e8db203c14423a84526649f
Abdoulfatah Kartish <abdoulfatah.kartish@msg.group>
2024-01-11 18:37:54 +0100

    Vite.config.js file undo

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/3D_portfolio/",
-  // build: {
-  //   rollupOptions: {
-  //     output: {
-  //       publicPath: "/3D_portfolio/"
-  //     }
-  //   }
-  // }
+  base: "/3D_portfolio/",
+  build: {
+    rollupOptions: {
+      output: {
+        publicPath: "/3D_portfolio/"
+      }
+    }
+  }
 });
